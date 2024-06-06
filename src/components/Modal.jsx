ecf0c31215443af9ad6be2cae2411bb03aa4c06f
unknown <omarelsayedmuhammad@gmail.com>
2024-06-06 16:25:39 +0300

    edited mobile screen with for modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ import { useWindowWith } from "../hooks/useWindowWith";
 
 export function Modal({ isOpen, gameStatus, setIsOpen }) {
   const width = useWindowWith();
-  const isMobile = width < 430;
+  const isMobile = width <= 430;
   const rulesImage =
     gameStatus === "simple" ? "/image-rules.svg" : "/image-rules-bonus.svg";
 
